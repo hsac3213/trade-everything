ad1861505eacad25316560e8f2d04509a8ae43e7
hsac3213 <hsac3213@gmail.com>
2025-10-24 20:12:48 +0900

    KIS 웹소켓 호가, 체결가 추가.

--- api-broker/kis/constants.py
+++ api-broker/kis/constants.py
@@ -1,2 +1,51 @@
 API_URL = "https://openapi.koreainvestment.com:9443"
 WS_URL = "ws://ops.koreainvestment.com:21000"
+
+# 컬럼 이름을 한국어로 변환
+COLUMN_TO_KOR_DICT = {
+    # 웹소켓 실시간 호가
+    "rsym": "실시간종목코드",
+    "symb": "종목코드",
+    "zdiv": "소숫점자리수",
+    "xymd": "현지일자",
+    "xhms": "현지시간",
+    "kymd": "한국일자",
+    "khms": "한국시간",
+    "bvol": "매수총잔량",
+    "avol": "매도총잔량",
+    "bdvl": "매수총잔량대비",
+    "advl": "매도총잔량대비",
+    "pbid1": "매수호가1",
+    "pask1": "매도호가1",
+    "vbid1": "매수잔량1",
+    "vask1": "매도잔량1",
+    "dbid1": "매수잔량대비1",
+    "dask1": "매도잔량대비1",
+
+    # 웹소켓 실시간 체결가
+    "SYMB": "종목코드",
+    "ZDIV": "수수점자리수",
+    "TYMD": "현지영업일자",
+    "XYMD": "현지일자",
+    "XHMS": "현지시간",
+    "KYMD": "한국일자",
+    "KHMS": "한국시간",
+    "OPEN": "시가",
+    "HIGH": "고가",
+    "LOW": "저가",
+    "LAST": "현재가",
+    "SIGN": "대비구분",
+    "DIFF": "전일대비",
+    "RATE": "등락율",
+    "PBID": "매수호가",
+    "PASK": "매도호가",
+    "VBID": "매수잔량",
+    "VASK": "매도잔량",
+    "EVOL": "체결량",
+    "TVOL": "거래량",
+    "TAMT": "거래대금",
+    "BIVL": "매도체결량",
+    "ASVL": "매수체결량",
+    "STRN": "체결강도",
+    "MTYP": "시장구분"
+}
--- api-broker/kis/price.py
+++ api-broker/kis/price.py
@@ -1,28 +1,127 @@
-from constants import WS_URL
+from constants import WS_URL, COLUMN_TO_KOR_DICT
 from ws_token_manager import get_ws_token
 import websockets
 import asyncio
+import json
 
-async def get_realtime_stock_price(stock_name):
-    # 해외주식(미국) 호가, 체결가, 체결통보
-    code_list = [['1','HDFFF010','DNASAAPL']]
+async def get_realtime_orderbook(stock_code):
+    async with websockets.connect(WS_URL + "/tryitout/HDFSASP0", ping_interval=30) as ws:
+        send_data = json.dumps({
+            "header": {
+                "approval_key": get_ws_token(),
+                "custtype": "P",
+                "tr_type": "1",
+                "content-type": "utf-8",
+            },
+            "body": {
+                "input": {
+                    "tr_id": "HDFSASP0",
+                    "tr_key": stock_code,
+                }
+            }
+        })
+        await ws.send(send_data)
 
-    senddata_list=[]
-    for i, j, k in code_list:
-        temp = '{"header":{"approval_key": "%s","custtype":"P","tr_type":"%s","content-type":"utf-8"},"body":{"input":{"tr_id":"%s","tr_key":"%s"}}}'%(get_ws_token(), i, j, k)
-        senddata_list.append(temp)
+        while True:
+            data = await ws.recv()
+            # 데이터는 0으로 시작
+            if data[0] == "0":
+                columns = [
+                    "rsym",
+                    "symb",
+                    "zdiv",
+                    "xymd",
+                    "xhms",
+                    "kymd",
+                    "khms",
+                    "bvol",
+                    "avol",
+                    "bdvl",
+                    "advl",
+                    "pbid1",
+                    "pask1",
+                    "vbid1",
+                    "vask1",
+                    "dbid1",
+                    "dask1"
+                ]
 
-    async with websockets.connect(WS_URL + "/tryitout/HDFFF010", ping_interval=None) as ws:
-        for senddata in senddata_list:
-            await ws.send(senddata)
-            print(f"Input Command is :{senddata}")
+                meta_data = data.split('|')[0]
+                real_data = data.split('|')[-1].split("^")
+
+                for col, value in zip(columns, real_data):
+                    print(f"{COLUMN_TO_KOR_DICT[col]} : {value}")
+                print("")
+            # 나머지는 그냥 json
+            else:
+                json_data = json.loads(data)
+                if(json_data["header"]["tr_id"] == "PINGPONG"):
+                    await ws.pong(data)
+                    print("Pong!")
+
+async def get_realtime_transaction_price(stock_code):
+    async with websockets.connect(WS_URL + "/tryitout/HDFSCNT0", ping_interval=30) as ws:
+        send_data = json.dumps({
+            "header": {
+                "approval_key": get_ws_token(),
+                "custtype": "P",
+                "tr_type": "1",
+                "content-type": "utf-8",
+            },
+            "body": {
+                "input": {
+                    "tr_id": "HDFSCNT0",
+                    "tr_key": stock_code,
+                }
+            }
+        })
+        await ws.send(send_data)
 
         while True:
             data = await ws.recv()
             print(data)
-            if data[0] == '0':
-                recvstr = data.split('|')
-                trid0 = recvstr[1]
-                print(trid0)
+            # 데이터는 0으로 시작
+            if data[0] == "0":
+                columns = [
+                    "SYMB",
+                    "ZDIV",
+                    "TYMD",
+                    "XYMD",
+                    "XHMS",
+                    "KYMD",
+                    "KHMS",
+                    "OPEN",
+                    "HIGH",
+                    "LOW",
+                    "LAST",
+                    "SIGN",
+                    "DIFF",
+                    "RATE",
+                    "PBID",
+                    "PASK",
+                    "VBID",
+                    "VASK",
+                    "EVOL",
+                    "TVOL",
+                    "TAMT",
+                    "BIVL",
+                    "ASVL",
+                    "STRN",
+                    "MTYP"
+                ]
 
-asyncio.run(get_realtime_stock_price("AAPL"))
+                meta_data = data.split('|')[0]
+                real_data = data.split('|')[-1].split("^")
+
+                for col, value in zip(columns, real_data):
+                    print(f"{COLUMN_TO_KOR_DICT[col]} : {value}")
+                print("")
+            # 나머지는 그냥 json
+            else:
+                json_data = json.loads(data)
+                if(json_data["header"]["tr_id"] == "PINGPONG"):
+                    await ws.pong(data)
+                    print("Pong!")
+
+#asyncio.run(get_realtime_orderbook("DNASAAPL"))
+#asyncio.run(get_realtime_transaction_price("DNASAAPL"))
