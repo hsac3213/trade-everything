724e975e69178196b3f495a77ff37340f6af4cd8
hsac3213 <hsac3213@gmail.com>
2025-11-11 08:35:46 +0900

    UPBit brocker 제거.

--- api_broker/UPBit/constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-API_URL = "https://api.upbit.com"
-
-WS_PRICE_URL = "wss://api.upbit.com/websocket/v1"
-WS_ASSET_URL = "wss://api.upbit.com/websocket/v1/private"
-
-APP_KEY = os.environ.get("UPBIT_APP")
-SEC_KEY = os.environ.get("UPBIT_SEC")
