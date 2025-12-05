f7b6afe5e6837c049ec596bda46c16fb49a27e33
hsac3213 <hsac3213@gmail.com>
2025-12-06 08:51:09 +0900

    KIS 주간거래 주문 기능 추가.

--- api_broker/KIS/order.py
+++ api_broker/KIS/order.py
@@ -15,43 +15,85 @@ def place_order(user_id, order):
         }
 
         tr_id = ""
+        resp_json = {}
 
-        # 매수 주문
-        if order["side"] == "BUY":
-            tr_id = "TTTT1002U"
-        # 매도 주문
-        elif order["side"] == "SELL":
-            tr_id = "TTTT1006U"
+        # 주간거래 시간 처리
+        if check_market_time(DAY_MARKET_TIME):
+            # 매수 주문
+            if order["side"] == "BUY":
+                tr_id = "TTTS6036U"
+            # 매도 주문
+            elif order["side"] == "SELL":
+                tr_id = "TTTS6037U"
+            else:
+                result["result"] = "Invalid side."
+                return result
+
+            payload = {
+                "CANO": get_key(user_id)["account_number_0"],
+                "ACNT_PRDT_CD": get_key(user_id)["account_number_1"],
+                "OVRS_EXCG_CD": "NASD",
+                "PDNO": str(order["symbol"]).upper(),
+                "ORD_QTY": str(order["quantity"]),
+                "OVRS_ORD_UNPR": str(order["price"]),
+                "ORD_SVR_DVSN_CD": "0",
+                # 주문구분 -> 00 : 지정가
+                # 주간거래는 지정가만 가능
+                # https://apiportal.koreainvestment.com/apiservice-apiservice?/uapi/overseas-stock/v1/trading/daytime-order
+                "ORD_DVSN": "00",
+            }
+
+            headers = {
+                "content-type": "application/json; charset=utf-8",
+                "authorization": "Bearer " + get_access_token(user_id),
+                "appkey": get_key(user_id)["app_key"],
+                "appsecret": get_key(user_id)["sec_key"],
+                "tr_id": tr_id,
+                "custtype": "P",
+            }
+
+            url = API_URL + f"/uapi/overseas-stock/v1/trading/daytime-order"
+            # POST 요청시 data가 아닌 json 파라미터로 요청 필요
+            resp = requests.post(url, json=payload, headers=headers, timeout=10)
+            pprint(resp.text)
+            resp_json = resp.json()
         else:
-            result["result"] = "Invalid side."
-            return result
+            # 매수 주문
+            if order["side"] == "BUY":
+                tr_id = "TTTT1002U"
+            # 매도 주문
+            elif order["side"] == "SELL":
+                tr_id = "TTTT1006U"
+            else:
+                result["result"] = "Invalid side."
+                return result
 
-        payload = {
-            "CANO": get_key(user_id)["account_number_0"],
-            "ACNT_PRDT_CD": get_key(user_id)["account_number_1"],
-            "OVRS_EXCG_CD": "NASD",
-            "PDNO": str(order["symbol"]).upper(),
-            "ORD_QTY": str(order["quantity"]),
-            "OVRS_ORD_UNPR": str(order["price"]),
-            "ORD_SVR_DVSN_CD": "0",
-            # 주문구분
-            "ORD_DVSN": "00",
-        }
+            payload = {
+                "CANO": get_key(user_id)["account_number_0"],
+                "ACNT_PRDT_CD": get_key(user_id)["account_number_1"],
+                "OVRS_EXCG_CD": "NASD",
+                "PDNO": str(order["symbol"]).upper(),
+                "ORD_QTY": str(order["quantity"]),
+                "OVRS_ORD_UNPR": str(order["price"]),
+                "ORD_SVR_DVSN_CD": "0",
+                # 주문구분
+                "ORD_DVSN": "00",
+            }
 
-        headers = {
-            "content-type": "application/json; charset=utf-8",
-            "authorization": "Bearer " + get_access_token(user_id),
-            "appkey": get_key(user_id)["app_key"],
-            "appsecret": get_key(user_id)["sec_key"],
-            "tr_id": tr_id,
-            "custtype": "P",
-        }
+            headers = {
+                "content-type": "application/json; charset=utf-8",
+                "authorization": "Bearer " + get_access_token(user_id),
+                "appkey": get_key(user_id)["app_key"],
+                "appsecret": get_key(user_id)["sec_key"],
+                "tr_id": tr_id,
+                "custtype": "P",
+            }
 
-        url = API_URL + f"/uapi/overseas-stock/v1/trading/order"
-        # POST 요청시 data가 아닌 json 파라미터로 요청 필요
-        resp = requests.post(url, json=payload, headers=headers, timeout=10)
-        pprint(resp.text)
-        resp_json = resp.json()
+            url = API_URL + f"/uapi/overseas-stock/v1/trading/order"
+            # POST 요청시 data가 아닌 json 파라미터로 요청 필요
+            resp = requests.post(url, json=payload, headers=headers, timeout=10)
+            #pprint(resp.text)
+            resp_json = resp.json()
 
         if "ODNO" in resp_json["output"]:
             result["result"] = "success"
@@ -61,7 +103,7 @@ def place_order(user_id, order):
         if "msg1" in resp_json:
             result["message"] = resp_json["msg1"]
 
-        pprint(result)
+        #pprint(result)
 
         return result
     except requests.exceptions.RequestException as e:
